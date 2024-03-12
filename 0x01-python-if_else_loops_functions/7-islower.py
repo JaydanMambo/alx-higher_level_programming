141a9d0e67ac08e7c815dd47bdaf63bfe863e874
JaydanMambo <jdanmambo@gmail.com>
2024-03-12 14:08:52 +0300

    Task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def islower(char):
-    return char >= 97 and char <= 122
+    return char >= ord('a') and char <= ord('z')
 
 
 if __name__ == "__main__":
     for char in range(ord('a'), ord('z') + 1):
         if islower(char):
-            print("{} is lower".format(char))
+            print("{:c} is lower".format(char))
         else:
-            print("{} is upper".format(char))
+            print("{:c} is upper".format(char))
